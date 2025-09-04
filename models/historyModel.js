efa001d68d5983d3a3dfdfa272120f71f2b3f47f
Anubhav Singh <anubhavsingh2027@gmail.com>
2025-09-05 03:01:17 +0530

    Update historyModel.js

--- models/historyModel.js
+++ models/historyModel.js
@@ -2,13 +2,14 @@ import mongoose from 'mongoose';
 
 const historySchema = new mongoose.Schema({
   userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  action: { type: String, required: true }, // e.g., 'Login', 'Analyze'
-  details: String, // Optional details
-  url: String,     // The URL that was analyzed
-  result: String,  // The result of the analysis (e.g., 'Safe', 'Phishing')
+  action: { type: String, required: true }, 
+  details: String, 
+  url: String,     
+  result: String,  
   timestamp: { type: Date, default: Date.now }
 });
 
 const History = mongoose.model('History', historySchema);
+
 
 export default History;
