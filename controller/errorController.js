89f38628b1664a93c050f3c2588ca68f2c5af704
Anubhav Singh <anubhavsingh2027@gmail.com>
2025-10-15 22:55:12 +0530

    Delete controller directory

--- controller/errorController.js
+++
@@ -1,6 +0,0 @@
-export const Errorpage404=(req, res) =>{ 
-  res.status(404).render('error')
-}
-export const Errorpage500=(req, res) =>{ 
-  res.status(500).render('error')
-}
--- controller/requiredController.js
+++
@@ -1,8 +0,0 @@
-export const requireLogin = (req, res, next) => {
-  if (req.session && req.session.userId) return next();
-  return res.redirect('/login');
-};
-export const requireLogined=(req,res,next)=>{
-  if (req.session && req.session.userId) return next();
-  return res.redirect('/login');
-}
