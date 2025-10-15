78ba5f3a19d406c0636e59e832b4d2fa758c07f5
Anubhav Singh <anubhavsingh2027@gmail.com>
2025-10-15 22:55:41 +0530

    Delete routers directory

--- routers/ErrorRouter.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-const ErrorRouter=express.Router();
-import {Errorpage404,Errorpage500} from '../controller/errorController.js'
-// ===== 404 handler =====
-ErrorRouter.get('/',Errorpage404);
-ErrorRouter.get('/',Errorpage500);
-
-export default ErrorRouter;
